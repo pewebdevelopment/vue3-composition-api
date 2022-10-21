32500dd8b8e142499ce50ec5e2ef3f602e32ed93
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-10-21 07:44:13 +0530

    Counter with the Options API

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,16 +1,54 @@
 <template>
     <div>
 
-<h1 class="Home">
-Home
-</h1>
+        <h1 class="Home">
+            Home
+        </h1>
+
     </div>
+
+    <br>
+
+    <div class="counter">
+
+        <button @click="decreaseValue">Decrease </button>
+        The present value is: {{counter}}
+        <button @click="increaseValue">Increase </button>
+
+
+    </div>
+
+
+
+
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+
+        return {
+
+            counter: 0,
+
+        }
+
+    },
+
+    methods: {
+        increaseValue: function () {
+            this.counter++;
+        },
+
+        decreaseValue: function () {
+            this.counter--;
+        }
     }
+
+
+}
+
+
 </script>
 
 <style lang="scss" scoped>
